5c206572ce34eea07cc5bd1042444f96aff4d10a
alimusana01 <alimusana68@gmail.com>
2021-04-02 13:52:56 +0700

    bikin file clas

--- projeck1.java
+++ projeck1.java
@@ -1,7 +1,7 @@
-public class Hello{
- public static void main(String [] args){
+public class projeck1{
+ public static void main (String [] args){
  
-	System.out.println("Hello dunia ");
+	System.out.println("Hello dunia");
 
  }
 }
